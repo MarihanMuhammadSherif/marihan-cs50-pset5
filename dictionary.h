7a8722863cf85f65e917530b3ac71e87b2c0c870
MarihanMuhammadSherif <mora_tony96@yahoo.com>
2016-08-22 04:30:16 +0200

    Create dictionary.h

---
+++ dictionary.h
@@ -0,0 +1,53 @@
+/**
+ * dictionary.h
+ *
+ * Computer Science 50
+ * Problem Set 5
+ *
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+#define HASHMAX 1000000
+
+typedef struct node
+{
+    
+    char string[LENGTH +1];
+    struct node* next; 
+    
+}node;
+
+node* hashtable[HASHMAX]={NULL};
+unsigned int dicsize=0;
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word);
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+#endif // DICTIONARY_H
